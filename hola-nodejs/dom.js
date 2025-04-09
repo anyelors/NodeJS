b3449762bcc4283cd3dd66daa41726dc0ac5efba
anyelors <anyelors@gmail.com>
2025-04-09 20:10:37 +0200

    Agregar ejercicios de DOM y eventos, incluyendo funciones para manipulación de texto y elementos en el HTML.

--- hola-nodejs/dom.js
+++ hola-nodejs/dom.js
@@ -1,3 +1,4 @@
+/** 
 function mostrarMensaje() {
     document.getElementById("mensaje").innerHTML = "<strong>Kaixo, DOM!</strong>";
   }
@@ -49,5 +50,95 @@ function validar() {
       error.textContent = "Email válido";
       error.classList.remove("error");
     }
+}
+*/
+
+/**
+ * EJERCICIOS DOM
+ */
+/**1. 
+function cambiarTexto() {
+  const text = document.getElementById("saludo");
+  const textButton = document.getElementById("cambiar");
+
+  if (text.innerHTML === "Kaixo") {
+    text.innerHTML = "Hola";
+    textButton.innerHTML = "Cambiar a Euskera";
+  } else {  
+    text.innerHTML = "Kaixo";
+    textButton.innerHTML = "Cambiar a Español";
+  }
+}
+*/
+
+/**2.  
+function resaltar() {
+  if (document.getElementById("destacado").classList.contains("amarrillo"))
+        document.getElementById("destacado").classList.remove("amarrillo");
+    else
+        document.getElementById("destacado").classList.add("amarrillo");
+}
+*/
+
+/**3. 
+function alternarVisibilidad() {
+  document.getElementById("info").classList.toggle("oculto");
+}
+*/
+
+/**4. 
+function cambiarNombre() {
+  document.getElementById("nombre").value = "Ane";
+}
+*/
+
+/**5. 
+function crearParrafo() {
+  const nuevoParrafo = document.createElement("p");
+  nuevoParrafo.textContent = "Este es un nuevo párrafo.";
+  document.body.appendChild(nuevoParrafo);
+}
+*/
+
+/**6. 
+function eliminarElemento() {
+  const elemento = document.getElementById("borrar");
+  if (elemento)
+    elemento.remove();
+}
+*/
+
+/**7. 
+function colorear() {
+  const textoParrafo = document.getElementById("texto");
+  textoParrafo.style.color = "Red";
+  textoParrafo.style.backgroundColor = "Yellow";
+}
+*/
+
+/**8. 
+function agregarElemento() {
+  const nuevoElemento = document.createElement("li");
+  nuevoElemento.textContent = "Nuevo elemento";
+  document.getElementById("miLista").appendChild(nuevoElemento);
+  if (document.getElementById("miLista").childElementCount %2 === 0) {
+    nuevoElemento.style.color = "blue";
+  } else {
+    nuevoElemento.style.color = "red";
+  }
+}
+*/
+
+/**9. */
+function mostrar() {
+  const element = document.getElementById("mensaje");
+
+  if (element.style.display === "block") {
+    element.style.display = "none";
+    document.getElementById("mostrar").textContent = "Mostrar mensaje";
+  } else {
+    element.style.display = "block";
+    document.getElementById("mostrar").textContent = "Ocultar mensaje";
+  }
 }
 

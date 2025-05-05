d234e1952c90c9c6b28738f3e489136ce9e49d4c
Anyelo Ramirez <anyelors@gmail.com>
2025-05-05 20:05:54 +0200

    Clase Async/Await #2

--- hola-nodejs/async_await.js
+++ hola-nodejs/async_await.js
@@ -1,4 +1,4 @@
-/**EJEMPLOS */
+/**EJEMPLOS 
 function algunaPromesa() {
     return new Promise((resolve, reject) => {
         setTimeout(() => {
@@ -67,4 +67,182 @@ try {
   
 obtenerUsuario();
 
+*/
+
+/**
+ * EJERCICIOS #####################################################################
+ */
+
+/**1. 
+function saludaUsuario() {
+  return new Promise((resolve) => {
+      setTimeout(() => {
+          resolve("¡Hola Usuario!");
+      }, 1000);
+  });
+}
+
+async function saludar() {
+  try {
+    let resultado = await saludaUsuario();
+    console.log(resultado);
+  } catch (error) {
+    console.error("Error:", error);
+  }
+}
+
+saludar();
+*/
+
+/**2. 
+function suma(n1, n2) {
+  return new Promise((resolve) => {
+      setTimeout(() => {
+          resolve(n1 + n2);
+      }, 1500);
+  });
+}
+
+async function sumarNumeros(num1, num2) {
+  try {
+    let resultado = await suma(num1, num2);
+    console.log(`La Suma de los numeros ${num1} y ${num2} es: ${resultado}`);
+  } catch (error) {
+    console.error("Error:", error);
+  }
+}
+
+sumarNumeros(2, 4);
+sumarNumeros(-5, 10);
+*/
+
+/**3.  
+function validaEdad(edad) {
+  return new Promise((resolve, reject) => {
+    (edad >= 18) ? resolve("Persona Mayor de Edad") : reject("Persona No es Mayor de Edad");
+  });
+}
+
+async function mayorEdad(edad) {
+  try {
+    let resultado = await validaEdad(edad);
+    console.log(`${resultado} [${edad}]`);
+  } catch (error) {
+    console.error("Error:", `${error} [${edad}]`);
+  }
+}
+
+mayorEdad(16);
+mayorEdad(22);
+*/
+
+/**4. 
+function esperar(ms) {
+  return new Promise(resolve => setTimeout(resolve, ms));
+}
+
+async function iniciar(num = 3) {
+  console.log("Inicio");
+  for (let index = num; index > 0; index--) {
+    console.log(index);
+    await esperar(1000);
+  }
+  console.log("Finaliza Cuenta regresiva");
+}
+
+iniciar(5);
+*/
+
+/**5.  
+function muestraFallo() {
+  return new Promise((_, reject) => {
+      setTimeout(() => {
+        reject("¡Ha fallado!");
+      }, 1500);
+  });
+}
+
+async function fallar() {
+  try {
+    await muestraFallo();
+  } catch (error) {
+    console.error("Error:", error);
+  }
+}
+
+fallar();
+/**/
+
+/**6. 
+function buscarUsuario() {
+  return new Promise((resolve) => {
+    console.log("Consultando Usuario");
+    setTimeout(() => {
+      resolve({ user: "admin", pass: "1234" });
+    }, 1500);      
+  });
+}
+
+let user = "admin";
+let pass = "1234";
+function processData(data) {
+  return new Promise((resolve, reject) => {
+      if (user == data.user && pass == data.pass)
+        resolve(`✅ Usuario [${data.user}] Bienvenido al Sistema!!`);
+      else
+        reject(`❌ Usuario [${user}] Credenciales inválidas`);
+  });
+}
+async function processDataPromise() {
+  try {
+    const datoBD = await buscarUsuario();
+    let resultado = await processData(datoBD);
+    console.log(`${resultado}`);
+  } catch (error) {
+    console.error("Error:", error);
+  }
+}
+
+processDataPromise();
+/**/
+
+/**7. */
+function generaNumero() {
+  return new Promise((resolve, reject) => {
+      setTimeout(() => {
+          const valNumero = Math.floor(Math.random() * 100);
+          resolve(valNumero);
+      }, 1000);
+  });
+}
+
+async function numAleatorio() {
+  try {
+    let resultado = await generaNumero();
+    console.log(`Número Generado: [${resultado}]`);
+  } catch (error) {
+    console.error("Error:", error);
+  }
+}
+
+numAleatorio();
+/**/
+
+/**8. 
+function esperar(ms) {
+  return new Promise(resolve => setTimeout(resolve, ms));
+}
+
+async function iniciar() {
+  console.log("Inicio");
+  await esperar(1000);
+  console.log("✅ Tarea Completada");
+  await esperar(1000);
+  console.log("✅ Tarea Completada");
+  await esperar(1000);
+  console.log("Finaliza Tareas");
+}
+
+iniciar();
+*/
 
